cbdf78326863d98a26841930110396896d3d3eaf
DEVA2516 <deva162501@gmail.com>
2022-09-29 16:53:07 +0530

    intercepter Implemented

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,32 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getAllUsers(){
-    return this.httpClient.get( environment.apiUrl+"/sql");
+  getAllUsers() {
+    return this.httpClient.get(environment.apiUrl + "/sql", {
+      headers: new HttpHeaders(
+        {
+          'Authorization': `bearer ${localStorage.getItem('token')}`
+        })
+    });
   }
 
-  createUser(data:object){
+  createUser(data: object) {
     //console.log(users);
-    return this.httpClient.post(environment.apiUrl+"/postData",data)
+    return this.httpClient.post(environment.apiUrl + "/postData", data)
   }
 
-  getUserById(id:number){
-    return this.httpClient.get(environment.apiUrl+"/getById?id="+id)
-  }
- 
-  updateUser(data:object){
-    return this.httpClient.put(environment.apiUrl+`/updatebyid`,data)
+  getUserById(id: number) {
+    return this.httpClient.get(environment.apiUrl + "/getById?id=" + id)
   }
 
-  deleteUser(userId:number){
-    return this.httpClient.delete(environment.apiUrl+`/deletebyid?id=${userId}`)
+  updateUser(data: object) {
+    return this.httpClient.put(environment.apiUrl + `/updatebyid`, data)
+  }
+
+  deleteUser(userId: number) {
+    return this.httpClient.delete(environment.apiUrl + `/deletebyid?id=${userId}`)
   }
 
 }
